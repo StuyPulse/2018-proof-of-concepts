acde9a7e82b13d95a15bf2868d95ae8f5637ca31
taaylor-m <so.timothy51@gmail.com>
2018-01-16 20:27:50 -0500

    Initialize new branch, add project files.

--- DEStinyMotorTest/src/org/usfirst/frc/team694/robot/OI.java
+++
@@ -1,9 +0,0 @@
-package org.usfirst.frc.team694.robot;
-
-public class OI {
-	public Gamepad driverPad;
-	
-	public OI() {
-		driverPad = new Gamepad(1);
-	}
-}
---
+++ src/org/usfirst/frc/team694/robot/Robot.java
@@ -0,0 +1,106 @@
+package org.usfirst.frc.team694.robot;
+
+import org.usfirst.frc.team694.robot.subsystems.Blender;
+import org.usfirst.frc.team694.robot.subsystems.Drivetrain;
+import org.usfirst.frc.team694.robot.subsystems.GearPusher;
+import org.usfirst.frc.team694.robot.subsystems.GearTrap;
+import org.usfirst.frc.team694.robot.subsystems.Shooter;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot extends IterativeRobot {
+
+	public static OI oi;
+
+	public static Drivetrain drivetrain;
+	public static GearPusher gearPusher;
+	public static GearTrap gearTrap;
+	public static Shooter shooter;
+	public static Blender blender;
+
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	@Override
+	public void robotInit() {
+		oi = new OI();
+		drivetrain = new Drivetrain();
+		gearPusher = new GearPusher();
+		gearTrap = new GearTrap();
+		shooter = new Shooter();
+		blender = new Blender();
+	}
+
+	/**
+	 * This function is called once each time the robot enters Disabled mode.
+	 * You can use it to reset any subsystem information you want to clear when
+	 * the robot is disabled.
+	 */
+	@Override
+	public void disabledInit() {
+
+	}
+
+	@Override
+	public void disabledPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select
+	 * between different autonomous modes using the dashboard. The sendable
+	 * chooser code works with the Java SmartDashboard. If you prefer the
+	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+	 * getString code to get the auto name from the text box below the Gyro
+	 *
+	 * You can add additional auto modes by adding additional commands to the
+	 * chooser code above (like the commented example) or additional comparisons
+	 * to the switch structure below with additional strings & commands.
+	 */
+	@Override
+	public void autonomousInit() {
+
+	}
+
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	@Override
+	public void autonomousPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	@Override
+	public void teleopInit() {
+		// This makes sure that the autonomous stops running when
+		// teleop starts running. If you want the autonomous to
+		// continue until interrupted by another command, remove
+		// this line or comment it out.
+	}
+
+	/**
+	 * This function is called periodically during operator control
+	 */
+	@Override
+	public void teleopPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	/**
+	 * This function is called periodically during test mode
+	 */
+	@Override
+	public void testPeriodic() {
+		LiveWindow.run();
+	}
+}
