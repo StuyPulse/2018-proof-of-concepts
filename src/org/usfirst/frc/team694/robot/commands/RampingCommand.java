18efa33e97a64c9c4a3eab431a6901221974f8e1
vs2961 <siuleungjing@gmail.com>
2018-01-31 15:11:17 -0500

    Update the Ramping Command

--- src/org/usfirst/frc/team694/robot/commands/RampingCommand.java
+++ src/org/usfirst/frc/team694/robot/commands/RampingCommand.java
@@ -3,13 +3,14 @@ package org.usfirst.frc.team694.robot.commands;
 import org.usfirst.frc.team694.robot.Robot;
 
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.PIDCommand;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
  */
-public class RampingCommand extends PIDCommand {
+public class RampingCommand extends Command {
 	private double time;
 	private double distance;
 	private int startingTime;
@@ -17,18 +18,16 @@ public class RampingCommand extends PIDCommand {
 	private double startSeconds;
 	private double speed;
     public RampingCommand(double distance, double time, double speed) {
-    	super(0,0,0);
     	this.startSeconds = 1.5;
     	requires(Robot.drivetrain);
     	this.distance = distance;
     	startingTime = (int) Timer.getFPGATimestamp();
     	this.time = time;
     	this.speed = speed;
- /*   	Robot.drivetrain.leftFront.configOpenloopRamp(startSeconds, 0);
+    	Robot.drivetrain.leftFront.configOpenloopRamp(startSeconds, 0);
     	Robot.drivetrain.rightFront.configOpenloopRamp(startSeconds, 0);
     	Robot.drivetrain.leftRear.configOpenloopRamp(startSeconds, 0);
-    	Robot.drivetrain.rightRear.configOpenloopRamp(startSeconds, 0);*/
-    	setSetpoint(0);
+    	Robot.drivetrain.rightRear.configOpenloopRamp(startSeconds, 0);
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     }
@@ -38,16 +37,12 @@ public class RampingCommand extends PIDCommand {
     	Robot.drivetrain.resetEncoders();
     	Robot.drivetrain.resetGyro();
     	System.out.println("Reset");
-    	this.getPIDController().setPID(
-				SmartDashboard.getNumber("RotateDegreesPID P", 0), 
-				SmartDashboard.getNumber("RotateDegreesPID I", 0), 
-				SmartDashboard.getNumber("RotateDegreesPID D", 0)
-				);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	this.timeElapsed = Timer.getFPGATimestamp() - startingTime;
+    	Robot.drivetrain.tankDrive(speed, speed);
 //    	System.out.println(Robot.drivetrain.getEncoderDistance());
 //    	System.out.println(Robot.drivetrain.getEncoderVelocity());
     	SmartDashboard.putNumber("Velocity", Robot.drivetrain.getEncoderVelocity());
@@ -69,15 +64,4 @@ public class RampingCommand extends PIDCommand {
     // subsystems is scheduled to run
     protected void interrupted() {
     }
-
-	@Override
-	protected double returnPIDInput() {
-		return Robot.drivetrain.getGyroAngle();
-	}
-
-	@Override
-	protected void usePIDOutput(double output) {
-			System.out.println("Running");
-			Robot.drivetrain.tankDrive(speed + output, speed - output);
-	}
 }
